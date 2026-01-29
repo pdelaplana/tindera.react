57bbcb33c9c697b68033a82086549556890aea00
pdelaplana <patrick.delaplana@gmail.com>
2026-01-30 06:10:50 +1000

    style: cleanup indents in IonText2

--- src/components/ui/IonText2.tsx
+++ src/components/ui/IonText2.tsx
@@ -4,33 +4,33 @@ import { IonText } from '@ionic/react';
 import type React from 'react';
 
 export interface IonText2Props {
-	children?: React.ReactNode;
-	fontSize?: string | number;
-	fontWeight?: string | number;
-	color?: string;
-	className?: string;
-	style?: React.CSSProperties;
+  children?: React.ReactNode;
+  fontSize?: string | number;
+  fontWeight?: string | number;
+  color?: string;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 export const IonText2: React.FC<IonText2Props> = ({
-	children,
-	fontSize,
-	fontWeight,
-	color,
-	className = '',
-	style = {},
+  children,
+  fontSize,
+  fontWeight,
+  color,
+  className = '',
+  style = {},
 }) => {
-	const combinedStyle: React.CSSProperties = {
-		...style,
-		...(fontSize && { fontSize: typeof fontSize === 'number' ? `${fontSize}px` : fontSize }),
-		...(fontWeight && { fontWeight }),
-	};
+  const combinedStyle: React.CSSProperties = {
+    ...style,
+    ...(fontSize && { fontSize: typeof fontSize === 'number' ? `${fontSize}px` : fontSize }),
+    ...(fontWeight && { fontWeight }),
+  };
 
-	return (
-		<IonText color={color} className={className} style={combinedStyle}>
-			{children}
-		</IonText>
-	);
+  return (
+    <IonText color={color} className={className} style={combinedStyle}>
+      {children}
+    </IonText>
+  );
 };
 
 export default IonText2;
