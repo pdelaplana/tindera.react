eea72031630fd9fdc5b594ccc7a0c5446f2217b6
pdelaplana <patrick.delaplana@gmail.com>
2026-02-12 07:29:44 +1000

    feat: update settings index with new page exports

--- src/features/settings/index.ts
+++ src/features/settings/index.ts
@@ -1,1 +1,8 @@
+export { default as DiscountTypeSettingsPage } from './pages/DiscountTypeSettingsPage';
+export { default as GlobalModifierGroupManagePage } from './pages/GlobalModifierGroupManagePage';
+export { default as GlobalModifiersPage } from './pages/GlobalModifiersPage';
+export { default as InventoryCategoriesPage } from './pages/InventoryCategoriesPage';
+export { default as ProductCategoriesPage } from './pages/ProductCategoriesPage';
 export { default as SettingsPage } from './pages/SettingsPage';
+export { default as TaxSettingsPage } from './pages/TaxSettingsPage';
+export { default as VoidRefundSettingsPage } from './pages/VoidRefundSettingsPage';
